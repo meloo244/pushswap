aa0d34906f50a1cc8402e5cd26576bf48c45c1f5
Mel <melvynoo143@gmail.com>
2023-10-08 23:27:11 +0200

    test

---
+++ libft/ft_bzero.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_bzero(void *s, size_t len)
+{
+	size_t			i;
+	unsigned char	*str2;
+
+	i = 0;
+	str2 = (unsigned char *)s;
+	while (i < len)
+	{
+		str2[i] = 0;
+		i++;
+	}
+	return (str2 = s);
+}
---
+++ libft/ft_memmove.c
@@ -0,0 +1,32 @@
+
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	size_t			i;
+	char			*dest_val;
+	char			*src_val;
+
+	dest_val = (char *)dest;
+	src_val = (char *)src;
+	if (len == 0)
+		return (dest);
+	if (src < dest)
+	{
+		while (len-- > 0)
+		{
+			dest_val[len] = src_val[len];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < len)
+		{
+			dest_val[i] = src_val[i];
+			i++;
+		}
+	}
+	return (dest);
+}
